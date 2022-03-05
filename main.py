6154d2a76c9fab6256995807904a9eb749daa126
颜琪滨 <2535367985@qq.com>
2022-03-05 19:09:26 +0800

    basically ui design

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+import sys
+
+
+class Loader:
+    def __init__(self):
+        self.ui = uic.loadUi("main_window.ui")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    # 显示窗口
+    loader = Loader()
+    loader.ui.show()
+    sys.exit(app.exec_())
